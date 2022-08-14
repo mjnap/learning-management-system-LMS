a285d6a25f8c23201656ede818bcdfd318c4b98f
Mohammad javad <njafimj@gmail.com>
2022-08-14 09:26:12 +0430

    انجام ریویو

--- src/main/java/ir/sobhan/lms/service/TermService.java
+++ src/main/java/ir/sobhan/lms/service/TermService.java
@@ -14,9 +14,7 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 
-import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
